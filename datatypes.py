25480edba9f41801c1d172c9c2a302c4a540b180
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2019-06-14 15:50:32 +0200

    added a file to handle custom data types and validators

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+from eve.io.base import BaseJSONEncoder
+from eve.io.mongo import Validator
+from uuid import UUID
+
+
+class UUIDEncoder(BaseJSONEncoder):
+    """ JSONEconder subclass used by the json render function.
+    This is different from BaseJSONEoncoder since it also addresses
+    encoding of UUID
+    """
+
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            return str(obj)
+        else:
+            # delegate rendering to base class method (the base class
+            # will properly render ObjectIds, datetimes, etc.)
+            return super(UUIDEncoder, self).default(obj)
+
+
+class UUIDValidator(Validator):
+    """
+    Extends the base mongo validator adding support for the uuid data-type
+    """
+
+    def _validate_type_uuid(self, value):
+        try:
+            UUID(value)
+            return True
+        except ValueError:
+            return False
+
