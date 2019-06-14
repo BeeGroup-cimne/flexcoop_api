da15440c1ddeca2b895f49f94367d993a397c514
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2019-06-14 15:50:14 +0200

    enable debug while developig

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ MONGO_DBNAME = os.environ['MONGO_DBNAME']
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 API_VERSION = "1"
 TRANSPARENT_SCHEMA_RULES = True
-
+DEBUG = True
 OAUTH_PROVIDERS = [
     "https://flexcoop-auth-server.okd.fokus.fraunhofer.de",
     "https://oauth2-flexcoop-keycloak.okd.fokus.fraunhofer.de/auth/realms/flexcoop"
