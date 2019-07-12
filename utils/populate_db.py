41cd705e3d2d5b5c0f76d2ffe5bb03c6e9cd31d4
Karsten Isakovic <karsten.isakovic@fokus.fraunhofer.de>
2019-07-12 09:46:11 +0200

    + Fixed 'delete' mechanism + Added response-output on error

--- utils/populate_db.py
+++ utils/populate_db.py
@@ -2,7 +2,7 @@ from requests import Session
 import json
 
 base_url = 'http://localhost:8080/1/'
-token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Ql80R2RaVXVaYWFvd0xDa08zQVBSYnhDUkNXMDY3Y3p2NEgwQk1wNWxZIn0.eyJqdGkiOiIyMGVkNTRlNi1hNzBlLTQ2ZTQtOGY2NS0zOTlmYjUyOTNmZTgiLCJleHAiOjE1NjI4OTE3MzEsIm5iZiI6MCwiaWF0IjoxNTYyODU1NzMxLCJpc3MiOiJodHRwczovL29hdXRoMi1mbGV4Y29vcC1rZXljbG9hay5va2QuZm9rdXMuZnJhdW5ob2Zlci5kZS9hdXRoL3JlYWxtcy9mbGV4Y29vcCIsImF1ZCI6ImJhY2tlbmQtbW9ja3VwIiwic3ViIjoiYTdhNWY3MGUtZTA1Yy00OWJiLThiNmQtOTkxYzQyOTc0NDkzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYmFja2VuZC1tb2NrdXAiLCJhdXRoX3RpbWUiOjE1NjI4NTU3MzEsInNlc3Npb25fc3RhdGUiOiJhNzU0ZjY4Ny04OTBlLTQ3NzgtODc1MS05ZmE2MGQxNTRiMWMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC8qIiwiaHR0cHM6Ly9mbGV4Y29vcC1iYWNrZW5kLW1vY2t1cC5va2QuZm9rdXMuZnJhdW5ob2Zlci5kZS8qIl0sInJlc291cmNlX2FjY2VzcyI6e30sInNjb3BlIjoib3BlbmlkIiwicm9sZSI6InByb3N1bWVyIn0.X2v_QMGu-gEb80UKGC7QJZiTsmOI2hLpi8mBk5_hkoDTkVFaOKgI3VDKHmhU4eo6Kcc4iAKp_QYz5xI5pzq8rewzmlml5rqcXUU0UukT9jW8iLX54PWKUCrU1Gc0IBAmL4rzRms7ji-xDDwHeJgyGxLWz_lavS7UKxxbjCOKuJMFXlk-y--tG_ujtck0VbKmeUtVnVP4h6aY3GWXN4MLRs2kJ2mnr1K2LnrOGYBfcXU7X0ifOmUtvGEF2VAgWN2AtHsRUe8llQ4FPwqLxj2qdgTRb6EpFlB1WPBTuMJOIWRMSw4COL3EtQmWz3nVPO3KNeqOz03YF3ufeYGyBTVZoQ'
+token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Ql80R2RaVXVaYWFvd0xDa08zQVBSYnhDUkNXMDY3Y3p2NEgwQk1wNWxZIn0.eyJqdGkiOiJiOTg4MWM5Mi1lNTZjLTQxZWUtOGNmMy02YjJmZTkwZTJhZGEiLCJleHAiOjE1NjI5NTE3MTAsIm5iZiI6MCwiaWF0IjoxNTYyOTE1NzEwLCJpc3MiOiJodHRwczovL29hdXRoMi1mbGV4Y29vcC1rZXljbG9hay5va2QuZm9rdXMuZnJhdW5ob2Zlci5kZS9hdXRoL3JlYWxtcy9mbGV4Y29vcCIsImF1ZCI6ImJhY2tlbmQtbW9ja3VwIiwic3ViIjoiYTdhNWY3MGUtZTA1Yy00OWJiLThiNmQtOTkxYzQyOTc0NDkzIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYmFja2VuZC1tb2NrdXAiLCJhdXRoX3RpbWUiOjE1NjI5MTU3MTAsInNlc3Npb25fc3RhdGUiOiJmMDg5ZDkxMS0xMjk3LTQ3ZTUtOTA4My03MmE0YzJmZmQwZWQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC8qIiwiaHR0cHM6Ly9mbGV4Y29vcC1iYWNrZW5kLW1vY2t1cC5va2QuZm9rdXMuZnJhdW5ob2Zlci5kZS8qIl0sInJlc291cmNlX2FjY2VzcyI6e30sInNjb3BlIjoib3BlbmlkIiwicm9sZSI6InByb3N1bWVyIn0.aICpHpG-XibcvRiSvhH_b1WbTpBqiRtb4GOiiPuqQWw-rMH-C9TBG2XrwQuveO36jUcLqki2vpsG4i1KxcSID4FurTnrlI_GjTlh35ZPrZj7RG-XJ5u576STiIFge3-FmcS0sRnPb4PxPum9d2sakTN2Tp9ZcCvLrRmBUnS6wBcixAenvdCwqX2lEO9fUiPEks02etRXjT0YDwEFeM-su9QM4wgLGGypZRFQuJNQyz-QbrvK6X0D2AzUGghp0Ncu0wXPO_F-QzL6-5IjRMCmYYmizYPYyvP7zW-edsRl6rt0yDMNWUim5blyaQ8-MypbF24iBZqoGbE4wWWvB6zXIg'
 headers = {'accept': 'application/xml', 'Authorization': token, "Content-Type": "application/json"}
 delete_first = True
 
@@ -14,15 +14,21 @@ with open("test_data.json") as json_file:
     json_data = json.load(json_file)
     # print(json_data)
 
+
 for domain in json_data:
+    if delete_first:
+        session.delete(base_url + domain)
+
     for item in json_data[domain]:
         # print(item)
-        if delete_first:
-            session.delete(base_url + domain)
         r = session.post(base_url + domain, data=json.dumps(item))
         # print(session.headers)
         print("HTTP status code: " + str(r.status_code) + " while connecting to " + base_url + domain)
-        print(json.dumps(item))
+        if r.status_code == 201:
+            print(json.dumps(item))
+        else:
+            print(r.text)
+        print('=================================================')
         # print(r.content)
 
 
