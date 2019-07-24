9d54bfdebfebf60a2943c9965f9921ca9ffb0298
Karsten Isakovic <karsten.isakovic@fokus.fraunhofer.de>
2019-07-24 12:07:59 +0200

    + DER endpoints with role=prosumer only access the accounts devices + DER endpoints with undefined role cant access data + Fixed logging info on Marketplace endpoint, removed filter

--- modules/der_registry/der_registry.py
+++ modules/der_registry/der_registry.py
@@ -1,10 +1,32 @@
 
+from jwt import JWT
+
+jwt = JWT()
 
 def pre_der_get_callback(request, lookup):
-    print('A GET request on the contacts endpoint has just been received!')
+    print('A GET request on a DER  endpoint has just been received!')
     print(request)
 
-    lookup["device_class"] = {'$eq': 'HVAC'}
-    print(lookup)
+    sub = None
+    role = None
+    if 'Authorization' in request.headers:
+        token = request.headers['Authorization']
+
+        try:
+            claim = jwt.decode(token, do_verify=False)
+            sub = claim['sub']
+            role = claim['role']
+
+        except jwt.exceptions.DecodeError as err:
+            print('exception decoding token exc=', err)
+
+    if role is None:
+        lookup["account_id"] = 'undefined'
+
+    elif role == 'prosumer':
+        lookup["account_id"] = sub
+
+    # lookup["device_class"] = {'$eq': 'HVAC'}
+    # print(lookup)
 
 
