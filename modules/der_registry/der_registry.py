00aeebddff121e68c554ce384889817f124a63c3
Karsten Isakovic <karsten.isakovic@fokus.fraunhofer.de>
2019-07-24 12:31:16 +0200

    + Fixed sub and role retrieval

--- modules/der_registry/der_registry.py
+++ modules/der_registry/der_registry.py
@@ -1,24 +1,40 @@
 
 from jwt import JWT
+from jwt.exceptions import JWTDecodeError
 
 jwt = JWT()
 
-def pre_der_get_callback(request, lookup):
-    print('A GET request on a DER  endpoint has just been received!')
-    print(request)
-
+def get_sub_and_role_from_request(request):
     sub = None
     role = None
-    if 'Authorization' in request.headers:
+    token = None
+    if 'Authorization' in request.headers and request.headers['Authorization'].startswith('Bearer '):
+        token = request.headers['Authorization'].split(None, 1)[1].strip()
+    elif 'Authorization' in request.headers:
         token = request.headers['Authorization']
 
+    if 'access_token' in request.form:
+        token = request.form['access_token']
+    elif 'access_token' in request.args:
+        token = request.args['access_token']
+
+    if token is not None:
         try:
             claim = jwt.decode(token, do_verify=False)
             sub = claim['sub']
             role = claim['role']
 
-        except jwt.exceptions.DecodeError as err:
+        except JWTDecodeError as err:
             print('exception decoding token exc=', err)
+
+    return sub, role
+
+
+def pre_der_get_callback(request, lookup):
+    print('A GET request on a DER  endpoint has just been received!')
+    print(request)
+
+    sub, role = get_sub_and_role_from_request(request)
 
     if role is None:
         lookup["account_id"] = 'undefined'
@@ -30,3 +46,6 @@ def pre_der_get_callback(request, lookup):
     # print(lookup)
 
 
+def pre_der_post_callback(request, lookup):
+    print('A POST request on a DER  endpoint has just been received!')
+    print(request)
