8a6ee3975c55b22a12b2dc3b8c6947694c3e6d91
Karsten Isakovic <karsten.isakovic@fokus.fraunhofer.de>
2019-07-24 12:30:32 +0200

    + Fixed logging info on Marketplace endpoint, removed filter

--- modules/marketplace/marketplace.py
+++ modules/marketplace/marketplace.py
@@ -1,6 +1,6 @@
 def pre_contract_get_callback(request, lookup):
-    print('A GET request on the contacts endpoint has just been received!')
+    print('A GET request on a Marketplace endpoint has just been received!')
     print(request)
 
-    lookup["status"] = {'$eq': 'active'}
-    print(lookup)
+#    lookup["status"] = {'$eq': 'active'}
+#    print(lookup)
