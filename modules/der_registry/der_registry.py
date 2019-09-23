d09a97f655670395db786e79f22168a2ce5401c5
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2019-09-23 14:19:53 +0200

    added comment to source of filters

--- modules/der_registry/der_registry.py
+++ modules/der_registry/der_registry.py
@@ -1,8 +1,9 @@
 
-
 def pre_der_get_callback(request, lookup):
     print('A GET request on the contacts endpoint has just been received!')
     print(request)
+
+    # lookup operators come from mongoDB see https://docs.mongodb.com/manual/reference/operator/query/
 
     lookup["device_class"] = {'$eq': 'HVAC'}
     print(lookup)
