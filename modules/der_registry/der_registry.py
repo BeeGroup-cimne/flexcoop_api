13e6e897cb3d772c2975ef3a7087af3d5d55ad2a
Karsten Isakovic <karsten.isakovic@fokus.fraunhofer.de>
2019-09-27 10:29:01 +0200

    Updated DER and Marketplace hooks (added service role and removed print's)

--- modules/der_registry/der_registry.py
+++ modules/der_registry/der_registry.py
@@ -3,52 +3,63 @@ import flask
 
 
 def pre_der_GET_callback(request, lookup):
-    print('A GET request on a DER  endpoint has just been received!')
-    print(request)
+    #print('A GET request on a DER  endpoint has just been received!')
+    #print(request)
 
     sub = request.sub
     role = request.role
 
-    if role is None:
-        print('unknown role')
-        flask.abort(403)
-
-    elif role == 'prosumer':
-        print('limiting results to account_id')
+    if role == 'prosumer':
+        #print('limiting results to account_id')
         lookup["account_id"] = sub
+
+    elif role == 'aggregator' or role == 'service':
+        pass
+
+    else:
+        print('error: GET DER unknown role ',role)
+        flask.abort(403)
 
 
 def pre_der_POST_callback(request):
-    print('A POST request on a DER  endpoint has just been received!')
-    print(request)
+    #print('A POST request on a DER  endpoint has just been received!')
+    #print(request)
 
-    sub = request.sub
     role = request.role
 
-    if role != 'prosumer':
-        print('role != prosumer')
+    if role == 'prosumer':
+        pass
+
+    else:
+        print('error: POST DER role != prosumer')
         flask.abort(403)
+
+
 
 
 def pre_flexibility_GET_callback(request, lookup):
-    print('A GET request on a Flexibility  endpoint has just been received!')
-    print(request)
+    #print('A GET request on a Flexibility  endpoint has just been received!')
+    #print(request)
 
     sub = request.sub
     role = request.role
 
-    if role is None:
-        print('unknown role')
-        flask.abort(403)
-
-    elif role == 'prosumer':
-        print('limiting results to account_id')
+    if role == 'prosumer':
+        #print('limiting results to account_id')
         lookup["account_id"] = sub
+
+    elif role == 'aggregator' or role == 'service':
+        pass
+
+    else:
+        print('error: GET flexibility unknown role ',role)
+        flask.abort(403)
 
 
 def pre_flexibility_POST_callback(request):
-    print('A POST request on a Flexibility  endpoint has just been received!')
-    print(request)
+    #print('A POST request on a Flexibility  endpoint has just been received!')
+    #print(request)
+    pass
 
 
 def set_hooks(app):
