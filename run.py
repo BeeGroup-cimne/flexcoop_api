978090d7fc974d8b341408b729b510ac283c9c43
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-10-26 12:56:46 +0200

    add proxy configuration

--- run.py
+++ run.py
@@ -10,8 +10,6 @@ from datatypes import UUIDEncoder, UUIDValidator
 from flexcoop_hooks import filter_internal_schema, filter_item_schema
 from auth.authentication import JWTokenAuth
 import modules
-from werkzeug.middleware.proxy_fix import ProxyFix
-
 from settings import NUM_PROXIES
 
 SWAGGER_URL = '/docs'
@@ -52,5 +50,4 @@ if __name__ == '__main__':
         HOST_PORT = environ['HOST_PORT']
     except KeyError:
         HOST_PORT = 8080
-    app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=NUM_PROXIES)
     app.run(port=int(HOST_PORT), host="0.0.0.0")
