266d930449282a488b067dc137cd2a53d8dbbcb6
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-10-31 17:28:26 +0100

    corrected devices

--- modules/devices/devices.py
+++ modules/devices/devices.py
@@ -5,7 +5,10 @@ import requests
 from flask import current_app
 from datetime import datetime
 
+from flexcoop_utils import get_middleware_token
+
 url = "https://<url>/gdem/newLDEM/{idLDEM}"
+cert = False #"/path/to/cert"
 
 def pre_devices_access_control_callback(request, lookup):
     account_id = request.account_id
@@ -38,7 +41,7 @@ def on_insterted_devices_callback(items):
     item = items[0] if len(items) > 0 else None
     if item:
         account_id = item['account_id']
-		aggregator_id = item['aggregator_id']
+        aggregator_id = item['aggregator_id']
     else:
         return
 
@@ -46,20 +49,22 @@ def on_insterted_devices_callback(items):
 
     ldem = ldm_collection.find_one({"account_id": account_id})
     if not ldem:
-		# If a new OSB is being registered, a new LDEM has to be instantiated containing all the DERs controlled by it
-		idLDEM = str(uuid.uuid1())
-		resp = requests.get(url.format(idLDEM))
-		if resp.status_code != 200:
-			flask.abort(403, "It has been not possible to register a new LDEM on the GDEM")
-		else:
-			ldm_collection.insert_one({
-				'ldem_id': idLDEM,
-				'account_id': account_id,
-				'aggregator_id': aggregator_id,
-				'creation_date': datetime.now(),
-				'timestamp': None,
-				'ders': devices
-			})		
+        # If a new OSB is being registered, a new LDEM has to be instantiated containing all the DERs controlled by it
+        idLDEM = str(uuid.uuid1())
+        headers = {"Authentication": get_middleware_token()}
+        resp = requests.get(url.format(idLDEM), headers=headers, verify=cert)
+        if not resp.ok:
+            # considere not aborting the request.
+            flask.abort(403, "It has been not possible to register a new LDEM on the GDEM")
+        else:
+            ldm_collection.insert_one({
+                'ldem_id': idLDEM,
+                'account_id': account_id,
+                'aggregator_id': aggregator_id,
+                'creation_date': datetime.now(),
+                'timestamp': None,
+                'ders': devices
+            })
     else:
         ldem['ders'] = devices
         ldm_collection.update({'ldem_id': ldem['ldem_id']}, {"$set": ldem})
