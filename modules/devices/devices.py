87524b9f1e9fbfd3900429df8486a497df7ae6cc
German <gmartinez.etraid@grupoetra.com>
2019-11-07 12:09:07 +0100

    GMN: -Fixed the hook triggered once new devices have been inserted -Included the schema of the LDEM

--- modules/devices/devices.py
+++ modules/devices/devices.py
@@ -7,7 +7,6 @@ from datetime import datetime
 
 from flexcoop_utils import get_middleware_token
 
-url = "https://<url>/gdem/newLDEM/{idLDEM}"
 cert = False #"/path/to/cert"
 
 def pre_devices_access_control_callback(request, lookup):
@@ -36,9 +35,6 @@ def on_update_devices_callback(updates, original):
 
 
 def on_insterted_devices_callback(items):
-    print(items)
-    return
-    #TODO: Correct this hook
     ldm_collection = current_app.data.driver.db['local_demand_manager']
     devices_collection = current_app.data.driver.db['devices']
     item = items[0] if len(items) > 0 else None
@@ -47,27 +43,20 @@ def on_insterted_devices_callback(items):
         aggregator_id = item['aggregator_id']
     else:
         return
-
+		
+	#To get all the identifiers of existing devices for that user
     devices = [x['device_id'] for x in devices_collection.find({"account_id": account_id})]
 
     ldem = ldm_collection.find_one({"account_id": account_id})
     if not ldem:
-        # If a new OSB is being registered, a new LDEM has to be instantiated containing all the DERs controlled by it
-        idLDEM = str(uuid.uuid1())
-        headers = {"Authentication": get_middleware_token()}
-        resp = requests.get(url.format(idLDEM), headers=headers, verify=cert)
-        if not resp.ok:
-            # considere not aborting the request.
-            flask.abort(403, "It has been not possible to register a new LDEM on the GDEM")
-        else:
-            ldm_collection.insert_one({
-                'ldem_id': idLDEM,
-                'account_id': account_id,
-                'aggregator_id': aggregator_id,
-                'creation_date': datetime.now(),
-                'timestamp': None,
-                'ders': devices
-            })
+		ldm_collection.insert_one({
+			'ldem_id': str(uuid.uuid1()),
+			'account_id': account_id,
+			'aggregator_id': aggregator_id,
+			'creation_date': datetime.now(),
+			'timestamp': None,
+			'ders': devices
+		})        
     else:
         ldem['ders'] = devices
         ldm_collection.update({'ldem_id': ldem['ldem_id']}, {"$set": ldem})
@@ -78,3 +67,4 @@ def set_hooks(app):
     app.on_pre_PATCH_devices += pre_devices_access_control_callback
     app.on_inserted_devices += on_insterted_devices_callback
     app.on_update_devices += on_update_devices_callback
+	
