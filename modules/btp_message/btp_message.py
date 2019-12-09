0ad6a7d30001d7bfd6012ef611943d131ab30c98
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-12-09 13:11:31 +0100

    etra corrections

--- modules/btp_message/btp_message.py
+++ modules/btp_message/btp_message.py
@@ -13,13 +13,11 @@ def pre_btp_message_access_control_callback(request, lookup):
     role = request.role
     aggregator_id = request.aggregator_id
     if role == 'prosumer':
-		flask.abort(403, "Prosumer can't have access to BTP messages")
-
+        flask.abort(403, "Prosumer can't have access to BTP messages")
     elif role == 'aggregator':
         lookup["aggregator_id"] = aggregator_id
-
     elif role == 'service':
-		if account_id not in ['GDEM']:
+        if account_id not in ['GDEM']:
             flask.abort(403, "The component {} can't have access to BTP messages".format(account_id))
     else:
         flask.abort(403, "Unknown user role")
@@ -27,4 +25,4 @@ def pre_btp_message_access_control_callback(request, lookup):
 def set_hooks(app):
     app.on_pre_GET_btp_message += pre_btp_message_access_control_callback
     app.on_pre_PATCH_btp_message += pre_btp_message_access_control_callback
-	app.on_pre_DELETE_btp_message += pre_btp_message_access_control_callback
+    app.on_pre_DELETE_btp_message += pre_btp_message_access_control_callback
