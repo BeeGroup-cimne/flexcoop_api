b5546764c772f9a27c85cd7db2c8957e0b082fdd
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2020-01-17 13:36:15 +0100

    add datapoints model

---
+++ modules/data_points/data_points.py
@@ -0,0 +1,41 @@
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+import uuid
+import flask
+
+from flask import current_app
+from datetime import datetime
+
+
+def pre_datapoints_access_control_callback(request, lookup):
+    account_id = request.account_id
+    role = request.role
+    aggregator_id = request.aggregator_id
+    if role == 'prosumer':
+        lookup["account_id"] = account_id
+
+    elif role == 'aggregator':
+        lookup["aggregator_id"] = aggregator_id
+
+    elif role == 'service':
+        pass
+    else:
+        flask.abort(403, "Unknown user role")
+
+
+def translate_device_output(response):
+    items = response['_items']
+    for item in items:
+        db_item = current_app.data.driver.db['data_points'].find_one({"device_id": item['device_id']})
+        item['device_class'] = db_item['rid']
+        item['reporting_metrics'] = []
+        print(item.keys())
+        for k, v in db_item['reporting_items'].items():
+            item['reporting_metrics'].append(k)
+
+
+
+def set_hooks(app):
+    app.on_pre_GET_data_points += pre_datapoints_access_control_callback
+    app.on_fetched_resource_data_points += translate_device_output
+
