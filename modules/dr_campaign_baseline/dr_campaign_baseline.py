048046b2b797d40616d1cba068858a93510654b4
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2020-01-23 13:00:41 +0100

    corrected endpoints and ACL

--- modules/dr_campaign_baseline/dr_campaign_baseline.py
+++ modules/dr_campaign_baseline/dr_campaign_baseline.py
@@ -13,17 +13,17 @@ def pre_dr_campaign_baseline_access_control_callback(request, lookup):
     role = request.role
     aggregator_id = request.aggregator_id
     if role == 'prosumer':
-		flask.abort(403, "Prosumer can't have access to DR Campaign baseline")
+        flask.abort(403, "Prosumer can't have access to DR Campaign baseline")
 
     elif role == 'aggregator':
         lookup["aggregator_id"] = aggregator_id
 
     elif role == 'service':
-		if account_id not in ['GDEM', 'DRSR']:
+        if account_id not in ['GDEM', 'DRSR']:
             flask.abort(403, "The component {} can't have access to DR Campaign baseline".format(account_id))
     else:
         flask.abort(403, "Unknown user role")
 
 def set_hooks(app):
     app.on_pre_GET_dr_campaign_baseline += pre_dr_campaign_baseline_access_control_callback    
-	app.on_pre_DELETE_dr_campaign_baseline += pre_dr_campaign_baseline_access_control_callback
+    app.on_pre_DELETE_dr_campaign_baseline += pre_dr_campaign_baseline_access_control_callback
--- modules/dr_campaign_timeline/dr_campaign_timeline.py
+++ modules/dr_campaign_timeline/dr_campaign_timeline.py
@@ -13,17 +13,17 @@ def pre_dr_campaign_timeline_access_control_callback(request, lookup):
     role = request.role
     aggregator_id = request.aggregator_id
     if role == 'prosumer':
-		flask.abort(403, "Prosumer can't have access to DR Campaign timeline")
+        flask.abort(403, "Prosumer can't have access to DR Campaign timeline")
 
     elif role == 'aggregator':
         lookup["aggregator_id"] = aggregator_id
 
     elif role == 'service':
-		if account_id not in ['GDEM', 'DRSR']:
+        if account_id not in ['GDEM', 'DRSR']:
             flask.abort(403, "The component {} can't have access to DR Campaign timeline".format(account_id))
     else:
         flask.abort(403, "Unknown user role")
 
 def set_hooks(app):
     app.on_pre_GET_dr_campaign_timeline += pre_dr_campaign_timeline_access_control_callback    
-	app.on_pre_DELETE_dr_campaign_timeline += pre_dr_campaign_timeline_access_control_callback
+    app.on_pre_DELETE_dr_campaign_timeline += pre_dr_campaign_timeline_access_control_callback
