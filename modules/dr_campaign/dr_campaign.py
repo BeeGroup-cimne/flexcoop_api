77776bbd0abd8fedc53e57b9e5bcfe0b5f31b248
Germán (ETRA) <gmartinez.etraid@grupoetra.com>
2020-01-24 12:24:44 +0100

    GMN: Included a hook on the dr_campaign.py for removing all the timelines attached to that DR Campaign when it is removed

--- modules/dr_campaign/dr_campaign.py
+++ modules/dr_campaign/dr_campaign.py
@@ -24,6 +24,21 @@ def pre_dr_campaign_access_control_callback(request, lookup):
     else:
         flask.abort(403, "Unknown user role")
 
+def on_deleted_dr_campaign_callback(item):
+    timeline_collection = current_app.data.driver.db['dr_campaign_timeline']	
+    if item:
+        dr_campaign_id = item['dr_campaign_id']
+    else:
+        return
+    # To get all the _etags of the dr_campaign_timelines attached to the removed dr_campaign
+    _etags = [x['_etag'] for x in timeline_collection.find({"dr_campaign_id": dr_campaign_id})]
+    if not _etags:
+		return
+	else
+		for _etag in _etags:
+			ldm_collection.deleteOne('dr_campaign_id': dr_campaign_id, '_etag': _etag)        
+		
 def set_hooks(app):
     app.on_pre_GET_dr_campaign += pre_dr_campaign_access_control_callback    
     app.on_pre_DELETE_dr_campaign += pre_dr_campaign_access_control_callback
+	app.on_deleted_dr_campaign += on_deleted_dr_campaign_callback
