2e470a19606c83458914ed8de0c47eb96156e33c
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2020-01-27 18:16:02 +0100

    dr_campaings corrections

--- modules/dr_campaign/dr_campaign.py
+++ modules/dr_campaign/dr_campaign.py
@@ -25,7 +25,7 @@ def pre_dr_campaign_access_control_callback(request, lookup):
         flask.abort(403, "Unknown user role")
 
 def on_deleted_dr_campaign_callback(item):
-    timeline_collection = current_app.data.driver.db['dr_campaign_timeline']	
+    timeline_collection = current_app.data.driver.db['dr_campaign_timeline']
     if item:
         dr_campaign_id = item['dr_campaign_id']
     else:
@@ -35,4 +35,5 @@ def on_deleted_dr_campaign_callback(item):
 def set_hooks(app):
     app.on_pre_GET_dr_campaign += pre_dr_campaign_access_control_callback    
     app.on_pre_DELETE_dr_campaign += pre_dr_campaign_access_control_callback
-    app.on_deleted_dr_campaign += on_deleted_dr_campaign_callback
+    app.on_pre_POST_dr_campaign += pre_dr_campaign_access_control_callback
+    app.on_delete_item_dr_campaign += on_deleted_dr_campaign_callback
